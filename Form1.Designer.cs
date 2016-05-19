96527784de0287bb5d48bd70024b0d0c0b81ff1d
Parker Bedlan <bedlansonny@gmail.com>
2016-05-19 12:35:39 -0500

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Risk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(12, 12);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(126, 99);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "BRAH";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(144, 38);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(126, 99);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "BRUH";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(144, 234);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(126, 48);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(276, 76);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(126, 99);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "BRO";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 327);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
